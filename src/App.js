3a29ea66eee95ffcc08b8b93277972d88ba6687d
keon <power3869@gmail.com>
2022-07-19 14:09:55 +0900

    use props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import HandIcon from './HandIcon.js';
 
 function App() {
-  return <div><HandIcon /></div>;
+  return (
+    <div>
+      <HandIcon value="rock" />
+      <HandIcon value="scissor" />
+      <HandIcon value="paper" />
+    </div>
+  );
 }
 
 export default App;
--- src/HandIcon.js
+++ src/HandIcon.js
@@ -1,7 +1,16 @@
-import rock from './assets/rock.svg';
+import rockImg from './assets/rock.svg';
+import scissorImg from './assets/scissor.svg';
+import paperImg from './assets/paper.svg';
 
-function HandIcon(){
-  return <img src={rock} />;
+const IMAGES = {
+  rock: rockImg,
+  scissor: scissorImg,
+  paper: paperImg,
+};
+
+function HandIcon({ value = 'rock' }){
+  const src = IMAGES[value];
+  return <img src={src} alt={value} />;
 }
 
 export default HandIcon;
